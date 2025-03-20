e44b9611ddfbdc3eacff9327f13fb7b16e3b2dd3
Anuj Choudhary <anuj.choudhary.2711@gmail.com>
2025-03-20 15:46:25 +0530

    UC7D committed

---
+++ Array Helper Func/First Occurrence.js
@@ -0,0 +1,50 @@
+// UC 7D - Find the first occurrence when Full Time Wage was earned using find function
+var WAGE_PER_HOUR = 20;
+var IS_PART_TIME = 1;
+var IS_FULL_TIME = 2;
+var PART_TIME_HOURS = 4;
+var FULL_TIME_HOURS = 8;
+
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+       totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}
+
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+
+function fulltimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+function mapDayWithWage(dailyWage) {
+    dailyCntr++;
+    return dailyCntr + " = " + dailyWage;
+}
+let dailyCntr = 0;
+let mapDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
+function findFulltimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+console.log("UC 7D - First time Fulltime wage was earned on Day: " + 
+    mapDayWithWageArr.find(findFulltimeWage));
