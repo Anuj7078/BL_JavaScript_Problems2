3ccb0e049cc36abb332aefebb35c42e2ae2f2455
Anuj Choudhary <anuj.choudhary.2711@gmail.com>
2025-03-20 14:48:35 +0530

    UC1 committed

---
+++ Employee Presence.js
@@ -0,0 +1,8 @@
+const is_Absent = 0
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == is_Absent) {
+    console.log("Employee is Absent");
+}
+else {
+    console.log("Employee is Present");
+}
