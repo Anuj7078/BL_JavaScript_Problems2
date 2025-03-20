4f74b63101f492b1c66210a15e0bce4b299c7a82
Anuj Choudhary <anuj.choudhary.2711@gmail.com>
2025-03-20 15:40:26 +0530

    UC7B commiited

---
+++ Array Helper Func/Day along with Daily Wage.js
@@ -0,0 +1,44 @@
+// UC 7B - Show the Day along with Daily Wage using Array map helper function
+var WAGE_PER_HOUR = 20;
+var IS_PART_TIME = 1;
+var IS_FULL_TIME = 2;
+var PART_TIME_HOURS = 4;
+var FULL_TIME_HOURS = 8;
+
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
+}
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+       totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}
+
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+
+let dailyCntr = 0;
+function mapDayWithWage(dailyWage) {
+    dailyCntr++;
+    return dailyCntr + " = " + dailyWage;
+}
+let mapDayWithWageArr = empDailyWageArr.map(mapDayWithWage);
+console.log("UC7B - Daily Wage Map");
+console.log(mapDayWithWageArr);
